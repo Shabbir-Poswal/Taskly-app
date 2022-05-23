8cde9675acad0f210fa9b799628e62bf95b0662d
Shabbir Poswal <shabbirposwal1@gmail.com>
2022-05-24 00:19:56 +0530

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'To do',
+      title: 'Taskly!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
